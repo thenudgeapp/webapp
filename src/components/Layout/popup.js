7e4cf898bd11a7e44c46f6e243176f3eb97e4497
ocheja <ocheja.patrick@gmail.com>
2023-02-13 09:22:32 +0100

    show cookie popup once and change use of cookies url to #

--- src/components/Layout/popup.js
+++ src/components/Layout/popup.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { UncontrolledAlert } from "reactstrap"
 import { Link } from 'react-router-dom'
 
+const SHOW_COOKIE_CONST = "cookie_shown"
+
 const Popup = () => {
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        if(!localStorage.getItem(SHOW_COOKIE_CONST)) {
+            setShow(true)
+        }
+    }, [])
     return (
-        <React.Fragment>
-            <UncontrolledAlert className="card cookie-popup shadow rounded py-3 px-4">
+        show ? <React.Fragment>
+            <UncontrolledAlert onClick={() => localStorage.setItem(SHOW_COOKIE_CONST, "do not show")}
+                               className="card cookie-popup shadow rounded py-3 px-4">
                 <p className="text-muted mb-0 fs-6">This website uses cookies to provide you with a great user experience. By using it,
-                    you accept our <Link to="https://themesbrand.com" target="_blank" rel="noopener noreferrer" className="text-success h6">use of cookies</Link></p>
+                    you accept our <Link to="#" target="_blank" rel="noopener noreferrer" className="text-success h6">use of cookies</Link></p>
                 <div className="cookie-popup-actions text-end">
                     {/* <button>
                         <i className="uil uil-times text-dark fs-4"></i>
@@ -15,7 +24,7 @@ const Popup = () => {
                 </div>
             </UncontrolledAlert>
 
-        </React.Fragment>
+        </React.Fragment> : <></>
     )
 }
 
